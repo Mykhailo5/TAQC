50a1817d9543ca07f32f34bea9dde8b9593e910e
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-07 20:35:36 +0300

    state

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int rez = 0;
         Scanner in = new Scanner(System.in);
-        System.out.println("input a");
-        int a = in.nextInt();
-        System.out.println("Input b");
-        int b = in.nextInt();
-        System.out.printf("rez = %d, %d, %d, %d,\n", a+b, a-b, a*b, a/b);
+        System.out.println("How are you?");
+        String answer = in.nextLine();
+        System.out.println("You are "+ answer);
     }
 }
