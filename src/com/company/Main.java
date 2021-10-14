2f4943d05997f47b6786185dde6a8e6ce67a60fc
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-14 21:54:24 +0300

    appendRepcaleCharAt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,35 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int rez = 0;
-        Scanner in = new Scanner(System.in);
-        System.out.println("input a");
-        int a = in.nextInt();
-        System.out.println("Input b");
-        int b = in.nextInt();
-        System.out.printf("rez = %d, %d, %d, %d,\n", a+b, a-b, a*b, a/b);
+        int x = 3;
+        int y = 56;
+        int z = x + y;
+        int t = x - y;
+        int r = x * y;
+        int pos;
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(x).append("+").append(y).append("=").append(z).append("\n");
+        strBuilder.append(x).append("-").append(y).append("=").append(t).append("\n");
+        strBuilder.append(x).append("*").append(y).append("=").append(r).append("\n");
+        System.out.println(strBuilder);
+        System.out.println("----------------");
+
+        while ((pos = strBuilder.indexOf("=")) != -1) {
+            strBuilder.deleteCharAt(pos);
+            strBuilder.insert(pos, " дорівнює ");
+        }
+        System.out.println(strBuilder);
+        System.out.println("----------------");
+
+
+        /*String value = strBuilder.toString().replace("=", " equals ");
+        System.out.println(value);*/
     }
 }
+
+
